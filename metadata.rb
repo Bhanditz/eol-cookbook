111507f1da2502bae55d202a0f9458e82541d43b
Anthony Goddard <anthony@anthonygoddard.com>
2014-01-28 11:36:49 -0500

    mysql converging

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
-name             'eol.org'
+name             'eol-cookbook'
 maintainer       'Woods Hole Marine Biological Laboratory'
 maintainer_email 'anthony@anthonygoddard.com'
 license          'Apache 2.0'
 description      'Installs/Configures eol.org'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+%w(apt mysql).each do |cb|
+  depends cb
+end

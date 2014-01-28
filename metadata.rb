9f8b71beb62cee84e9cd587193a06429c22dc6fb
Anthony Goddard <anthony@anthonygoddard.com>
2014-01-28 10:08:01 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'eol.org'
+maintainer       'Woods Hole Marine Biological Laboratory'
+maintainer_email 'anthony@anthonygoddard.com'
+license          'Apache 2.0'
+description      'Installs/Configures eol.org'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'

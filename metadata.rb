77ba509ca928bea22001797f9c1c9e7520f04071
Nathan Wilson <nathan@collectivesource.com>
2014-01-28 16:43:01 +0000

    Fixed some eol.org to eol-cookbook changes

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'eol-cookbook'
 maintainer       'Woods Hole Marine Biological Laboratory'
 maintainer_email 'anthony@anthonygoddard.com'
 license          'Apache 2.0'
-description      'Installs/Configures eol.org'
+description      'Installs/Configures eol-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
